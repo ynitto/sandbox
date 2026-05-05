c51beacf9ab71db7e1585e0d68b24297c70918ee
ynitto7 <ynitto7@gmail.com>
2026-05-05 09:43:54 +0900

    feat: add textlint configuration and installation script

---
+++ obsidian-extensions/obsidian-textlint/install.sh
@@ -0,0 +1,20 @@
+npm install -g textlint
+
+# rules
+npm install -g @textlint-ja/textlint-rule-no-synonyms sudachi-synonyms-dictionary
+npm install -g @textlint-ja/textlint-rule-no-dropping-i
+npm install -g @textlint-ja/textlint-rule-no-insert-dropping-sa
+npm install -g textlint-rule-no-mixed-zenkaku-and-hankaku-alphabet
+npm install -g textlint-rule-period-in-list-item
+npm install -g textlint-rule-prefer-tari-tari
+npm install -g textlint-rule-preset-ja-technical-writing
+npm install -g textlint-rule-date-weekday-mismatch
+npm install -g textlint-rule-ja-no-orthographic-variants
+npm install -g textlint-rule-write-good
+npm install -g textlint-rule-preset-ai-writing
+npm install -g textlint-rule-incremental-headers
+npm install -g textlint-rule-aws-service-name
+npm install -g textlint-rule-max-comma
+
+# filters
+npm install -g textlint-filter-rule-comments
